60f462eb0a3f0ed1759e09e20ae07a84e479afcc
RJ <rmjackson234@gmail.com>
2026-02-21 20:53:11 -0700

    moved aerospace in

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,3 +91,5 @@ vim.cmd('packadd! nohlsearch')
 vim.o.guifont = "SF Mono:h17"
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.cmd('colorscheme kanagawa')
+vim.cmd('set lines=50')
+vim.cmd('set columns=125')
